e66598e5779400f1ff0c2b598ac95a57f19c130c
Kevin Conley <kevin.conley@intel.com>
2018-05-08 00:21:01 -0700

    Switch to geckodriver

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,5 @@
 import pytest
 
-@pytest.fixture
-def chrome_options(chrome_options):
-    # See https://docs.travis-ci.com/user/chrome#Sandboxing
-    chrome_options.add_argument('--no-sandbox')
-    return chrome_options
-
 def test_index(selenium, base_url):
     selenium.get(base_url)
     assert 'Utility Knife' == selenium.title
