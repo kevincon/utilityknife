8db3f6383ab14bd1049fe7a76d26f6623d334ce3
Kevin Conley <kevin.conley@intel.com>
2018-05-08 00:04:32 -0700

    Add basic Selenium test using chromedriver

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
 import pytest
 
-def test_fail():
-    assert False
+@pytest.fixture
+def chrome_options(chrome_options):
+    # See https://docs.travis-ci.com/user/chrome#Sandboxing
+    chrome_options.add_argument('--no-sandbox')
+    return chrome_options
+
+def test_index(selenium, base_url):
+    selenium.get(base_url)
+    assert 'Utility Knife' == selenium.title
